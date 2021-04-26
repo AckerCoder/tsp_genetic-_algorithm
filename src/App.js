a9f1b023593175d9c36a98a1b093a30c795e700f
Miguel <ackercoder@gmail.com>
2021-04-26 17:46:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Graph } from "react-d3-graph";
+
+import {cities, roads, shortestRoad} from './utils';
+
+const App = () => {
+
+  const formatRoads = (roads)=>{
+    const newRoads = [];
+    for(let i = 0; i < roads.length; i++){
+      newRoads.push(roads[i][0]);
+    }
+    return newRoads;
+  }
+
+  const newRoads = formatRoads(roads);
+
+  const data = {
+    nodes: cities,
+    links: newRoads
+  };
+
+  const dataShortestPath = {
+    nodes: cities,
+    links: shortestRoad
+  }
+
+  
+
+  const onClickNode = function(nodeId) {
+    window.alert(`Clicked node ${nodeId}`);
+  };
+  
+  const onClickLink = function(source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };
+  
+
+  const myConfig = {
+    nodeHighlightBehavior: true,
+    node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+    },
+    link: {
+      highlightColor: "lightblue",
+    },
+  };
+  
+  const myConfigSolution = {
+    nodeHighlightBehavior: true,
+    directed:true,
+    node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+    },
+    link: {
+      highlightColor: "lightblue",
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Graph
+        id="graph-id" // id is mandatory
+        data={data}
+        config={myConfig}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />
+      <Graph
+        id="solution" // id is mandatory
+        data={dataShortestPath}
+        config={myConfigSolution}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />;
     </div>
   );
 }
